9aefc63d6046780617fe59f26aba4cce7abf8341
Ronny Chan <ronny6993@gmail.com>
2018-04-28 03:09:54 -0400

    css changes

--- seiri-client-internals/src/View.tsx
+++ seiri-client-internals/src/View.tsx
@@ -1,63 +1,68 @@
 import * as React from "react";
 import { DebounceInput } from "react-debounce-input";
-import { connect, Dispatch } from 'react-redux';
+import { connect, Dispatch } from "react-redux";
 import { updateQuery, updateTracksTick } from "./actions";
 import State from "./State";
 import TrackTable from "./TrackTable";
 import { Track } from "./types";
-import "./View.css"
+import "./View.css";
 
 interface ViewProps {
-    tracks: Track[],
-    query: string,
-    dispatch?: Dispatch<any>
+  tracks: Track[];
+  query: string;
+  dispatch?: Dispatch<any>;
 }
 
-const mapStateToProps = (state: State) : ViewProps => {
-    return { tracks: state.tracks, query: state.query }
-}
+const mapStateToProps = (state: State): ViewProps => {
+  return { tracks: state.tracks, query: state.query };
+};
 
-const mapDispatchToProps = (dispatch: Dispatch<any>, ownProps: ViewProps) : ViewProps => {
-  return { ...ownProps, dispatch}
-}
+const mapDispatchToProps = (
+  dispatch: Dispatch<any>,
+  ownProps: ViewProps
+): ViewProps => {
+  return { ...ownProps, dispatch };
+};
 
 // tslint:disable:jsx-no-lambda
 class View extends React.Component<ViewProps> {
+  private queryInput: HTMLInputElement | null;
   constructor(props: ViewProps) {
-    super(props)
+    super(props);
     window.setTimeout(() => {
-      this.props.dispatch!(updateQuery.action({query: ""}))
-      this.props.dispatch!(updateTracksTick.action())
-    }, 0)
+      this.props.dispatch!(updateQuery.action({ query: "" }));
+      this.props.dispatch!(updateTracksTick.action());
+    }, 0);
+    window.addEventListener('keydown', () => this.queryInput!.focus())
   }
 
   public render() {
     return (
       <div className="container">
         <div className="tracks-containers">
-          <TrackTable tracks={this.props.tracks}/>
+          <TrackTable tracks={this.props.tracks} />
         </div>
-        <DebounceInput
-          className="bang-input"
-          minLength={1}
-          debounceTimeout={100}
-          // tslint:disable-next-line:no-console
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+        <div className="main-bar">
+          <DebounceInput
+            inputRef={(input) => { this.queryInput = input; }}
+            className="bang-input"
+            minLength={1}
+            debounceTimeout={100}
             // tslint:disable-next-line:no-console
-            this.props.dispatch!(updateQuery.action({query: e.target.value}))
-          }}
-        />
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              // tslint:disable-next-line:no-console
+              this.props.dispatch!(
+                updateQuery.action({ query: e.target.value })
+              );
+            }}
+          />
+          <button className="btn-quit" onClick={() => close()}>
+            &#xe711;
+          </button>
+        </div>
       </div>
     );
   }
-
-  // private launch(filename: string) {
-  //   // tslint:disable-next-line:no-console
-  //   const dirname = window.require("path").dirname(filename)
-  //   // tslint:disable-next-line:no-console
-  //   console.log(dirname)
-  //   window.require("child_process").execFile(dirname)
-  // }
 }
 
 export default connect<ViewProps>(mapStateToProps, mapDispatchToProps)(View);
--- seiri-client/main.js
+++ seiri-client/main.js
@@ -8,7 +8,7 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600, frame: false})
   
     // and load the index.html of the app.
     win.loadURL(url.format({
