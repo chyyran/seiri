d1bc9249ebbc234a4bfef4a6d68ebfab2beac271
Ronny Chan <ronny6993@gmail.com>
2018-05-02 12:13:30 -0400

    ensure single instance

--- seiri-client/main.js
+++ seiri-client/main.js
@@ -10,34 +10,64 @@ let win = null;
 let winClose = null;
 let tray = null;
 let runningWatcher = null;
-let isQuitting = false;
+let watcherShouldQuit = false;
 const newTracksAdded = [];
 
-const processWatcherMessage = (message) => {
-  let messageType = message.split('~')[0];
+const shouldQuit = app.makeSingleInstance(function(
+  commandLine,
+  workingDirectory
+) {
+  // Someone tried to run a second instance, we should focus our window.
+  if (winClose && win !== null) {
+    clearTimeout(winClose);
+    winClose = null;
+    win.show();
+  } else if (win === null) {
+    createWindow();
+    win.once("ready-to-show", () => {
+      win.show();
+    });
+  } else {
+    win.show();
+  }
+});
+
+if (shouldQuit) {
+  watcherShouldQuit = true;
+  app.quit();
+  return;
+}
+
+const processWatcherMessage = message => {
+  let messageType = message.split("~")[0];
   let messagePayload = message.slice(message.indexOf("~") + 1);
-  let messageMessage = messagePayload.split(message.indexOf(":") + 1)[0];
   switch (messageType) {
     case "TRACKMOVEERR":
-      console.log("Track move error...")
+      console.log("Track move error...");
       notifier.notify({
         title: "Track Move Error",
-        message: "Error occurred when moving " + messageMessage
-      })
+        message: "Error occurred when moving " + messagePayload
+      });
+      break;
     case "MISSINGTAG":
-      console.log("Missing tag...")
+      console.log("Missing tag...");
       notifier.notify({
         title: "Track is missing tag.",
         message: messagePayload
-      })
+      });
+      break;
     case "TRACKADDED":
       console.log("Track added...");
-      newTracksAdded.push(messageMessage);
+      newTracksAdded.push(messagePayload);
       break;
     default:
-      console.log("Unknown message...")
+      notifier.notify({
+        title: "Error occurred.",
+        message: messagePayload
+      });
+      break;
   }
-}
+};
 
 const startNewTracksNotifier = () => {
   setInterval(() => {
@@ -45,26 +75,31 @@ const startNewTracksNotifier = () => {
       notifier.notify({
         title: "New Tracks Added",
         message: "Added " + newTracksAdded[0]
-      })
+      });
       newTracksAdded.length = 0;
     } else if (newTracksAdded.length !== 0) {
       notifier.notify({
         title: "New Tracks Added",
-        message: "Added " + newTracksAdded[0] + " and " + (newTracksAdded.length - 1) + " more."
-      })
+        message:
+          "Added " +
+          newTracksAdded[0] +
+          " and " +
+          (newTracksAdded.length - 1) +
+          " more."
+      });
       newTracksAdded.length = 0;
     }
-  }, 60000)
-}
+  }, 60000);
+};
 
 const restartWatcher = () => {
   console.log("Starting watcher...");
   runningWatcher = watcher(
     chunk => {
-      processWatcherMessage(chunk.toString("utf8"))
+      processWatcherMessage(chunk.toString("utf8"));
     },
     chunk => {
-      if (!isQuitting) {
+      if (!watcherShouldQuit) {
         console.log("Watcher unexpectedly quit, restarting...");
         restartWatcher();
       }
@@ -76,10 +111,11 @@ app.on("ready", () => {
   Menu.setApplicationMenu(null);
   restartWatcher();
   startNewTracksNotifier();
-  tray = new Tray(__dirname + '/branding/seiri.png')
+  tray = new Tray(__dirname + "/branding/seiri.png");
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: 'Show Library', click: () => {
+      label: "Show Library",
+      click: () => {
         console.log("Show");
         if (winClose && win !== null) {
           clearTimeout(winClose);
@@ -87,41 +123,42 @@ app.on("ready", () => {
           win.show();
         } else if (win === null) {
           createWindow();
-          win.once('ready-to-show', () => {
-            win.show()
-          })
+          win.once("ready-to-show", () => {
+            win.show();
+          });
         } else {
           win.show();
         }
       }
     },
     {
-      label: 'Restart Folder Watcher', click: () => {
+      label: "Restart Folder Watcher",
+      click: () => {
         if (runningWatcher) {
           runningWatcher.quit();
           console.log("Sent exit signal to running watcher...");
         }
       }
     },
-    { type: 'separator' },
-    { label: 'Quit', role: 'quit' },
-  ])
-  tray.setToolTip('seiri is running.')
+    { type: "separator" },
+    { label: "Quit", role: "quit" }
+  ]);
+  tray.setToolTip("seiri is running.");
   tray.setContextMenu(contextMenu);
-  contextMenu.on('menu-will-show', () => {
-    console.log('menu will show!');
-  })
-  tray.on('click', () => {
-    console.log('Restored!');
+  contextMenu.on("menu-will-show", () => {
+    console.log("menu will show!");
+  });
+  tray.on("click", () => {
+    console.log("Restored!");
     if (winClose && win !== null) {
       clearTimeout(winClose);
       winClose = null;
       win.show();
     } else if (win === null) {
       createWindow();
-      win.once('ready-to-show', () => {
+      win.once("ready-to-show", () => {
         win.show();
-      })
+      });
     } else {
       win.show();
     }
@@ -130,7 +167,13 @@ app.on("ready", () => {
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, frame: false, icon: __dirname + '/branding/seiri.png', show: false });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    icon: __dirname + "/branding/seiri.png",
+    show: false
+  });
 
   // and load the index.html of the app.
   win.loadURL(
@@ -170,7 +213,7 @@ app.on("ready", () => {
 });
 
 app.on("quit", () => {
-  isQuitting = true;
+  watcherShouldQuit = true;
   if (runningWatcher) {
     runningWatcher.quit();
     console.log("Sent exit signal to running watcher...");
@@ -184,15 +227,13 @@ app.on("activate", () => {
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow();
-    win.once('ready-to-show', () => {
-      win.show()
-    })
+    win.once("ready-to-show", () => {
+      win.show();
+    });
   }
 });
 
-app.on('window-all-closed', () => {
-
-});
+app.on("window-all-closed", () => {});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
