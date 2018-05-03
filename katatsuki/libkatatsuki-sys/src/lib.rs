8218ea5386a9189a2dcbf3f89178d9c8a1a01dbc
Ronny Chan <ronny6993@gmail.com>
2018-05-03 17:54:48 -0400

    try again this time with explicit link attr

--- katatsuki/libkatatsuki-sys/src/lib.rs
+++ katatsuki/libkatatsuki-sys/src/lib.rs
@@ -25,8 +25,8 @@ pub struct katatsuki_Track {
 }
 
 // #[link(name = "libkatatsuki", kind = "static")]
-// #[link(name = "bootstrapperdll", kind = "static")]
-// #[link(name = "Runtime",  kind = "static")]
+#[link(name = "bootstrapperdll", kind = "static")]
+#[link(name = "Runtime",  kind = "static")]
 extern "C" {
     pub fn katatsuki_get_track_data(file_path: *const u16) -> katatsuki_Track;
 }
