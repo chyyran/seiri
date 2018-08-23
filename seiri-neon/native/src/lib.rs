f9016d20a36b4a0d3f70236a54bde8c98dbc26bf
Ronny Chan <ronny6993@gmail.com>
2018-08-23 00:53:30 -0400

    update to neon 0.2

--- seiri-neon/native/src/lib.rs
+++ seiri-neon/native/src/lib.rs
@@ -1,11 +1,11 @@
 #[macro_use]
 extern crate neon;
-extern crate seiri;
 extern crate num_traits;
-
-use neon::js::{JsArray, JsBoolean, JsInteger, JsNull, JsObject, JsString, JsUndefined, Object};
-use neon::vm::Throw;
-use neon::vm::{Call, JsResult};
+extern crate seiri;
+use neon::prelude::*;
+// use neon::{JsArray, JsBoolean, JsNumber, JsNull, JsObject, JsString, JsUndefined, Object};
+// use neon::vm::Throw;
+// use neon::vm::{FunctionContext, JsResult};
 use num_traits::cast::ToPrimitive;
 use seiri::config::get_config;
 use seiri::database;
@@ -16,17 +16,23 @@ use seiri::TrackFileType;
 use std::path::Path;
 
 #[allow(non_snake_case)]
-fn refresh_tracks(call: Call) -> JsResult<JsUndefined> {
-    let scope = call.scope;
+fn refresh_tracks(mut ctx: FunctionContext) -> JsResult<JsUndefined> {
     let config = get_config();
     let conn = database::get_database_connection();
     let library_path = Path::new(&config.music_folder);
-    let args = &call.arguments.require(scope, 0)?.check::<JsArray>()?;
+
+    let args = ctx.argument::<JsArray>(0)?;
+
     let mut track_filenames: Vec<String> = Vec::new();
     for i in 0..args.len() {
-        let result = args.get(scope, i)?.check::<JsString>()?.value();
-        track_filenames.push(result.to_owned());
+        let result = args
+            .get(&mut ctx, i)?
+            .downcast::<JsString>()
+            .or_throw(&mut ctx)?
+            .value();
+        track_filenames.push(result);
     }
+
     for file in track_filenames {
         let tracks = database::query_tracks(Bang::FilePath(file.clone()), &conn, None, None);
         if let Ok(tracks) = tracks {
@@ -41,76 +47,101 @@ fn refresh_tracks(call: Call) -> JsResult<JsUndefined> {
                         println!("RECONSIDERED NOT FOUND {:?}", track);
                         database::remove_track(&track, &conn);
                     }
-                    Err(_) => panic!("Error when reconsidering {}. Is tools set up correctly?", file),
+                    Err(_) => panic!(
+                        "Error when reconsidering {}. Is tools set up correctly?",
+                        file
+                    ),
                 }
             }
         }
     }
-    Ok(JsUndefined::new())
+    Ok(ctx.undefined())
 }
 
 #[allow(non_snake_case)]
-fn query_tracks(call: Call) -> JsResult<JsObject> {
-    let scope = call.scope;
-    let ret = JsObject::new(scope);
-    let query = &call.arguments
-        .require(scope, 0)?
-        .check::<JsString>()?
-        .value();
-    let bang = Bang::new(query).unwrap();
+fn query_tracks(mut ctx: FunctionContext) -> JsResult<JsObject> {
+    let ret = ctx.empty_object();
+
+    let query = ctx.argument::<JsString>(0)?.value();
+
+    let bang = Bang::new(&query).unwrap();
     let conn = database::get_database_connection();
     let results: Vec<Track> = database::query_tracks(bang, &conn, None, None).unwrap();
-    let jsTracks = JsArray::new(scope, results.len() as u32);
-    for (i, track) in results.into_iter().enumerate() {
-        let mut jsTrack = JsObject::new(scope);
-        jsTrack.set("filePath", JsString::new(scope, &track.file_path.into_os_string().into_string().unwrap()).unwrap())?;
-        jsTrack.set("title", JsString::new(scope, &track.title).unwrap())?;
-        jsTrack.set("artist", JsString::new(scope, &track.artist).unwrap())?;
+    let jsTracks = ctx.empty_array();
 
-        let jsAlbumArtists = JsArray::new(scope, track.album_artists.len() as u32);
+    for (i, track) in results.into_iter().enumerate() {
+        let mut jsTrack = ctx.empty_object();
+        let filePath = ctx.string(&track.file_path.into_os_string().into_string().unwrap());
+        jsTrack.set(&mut ctx, "filePath", filePath)?;
+
+        let title = ctx.string(&track.title);
+        jsTrack.set(&mut ctx, "title", title)?;
+
+        let artist = ctx.string(&track.artist);
+        jsTrack.set(&mut ctx, "artist", artist)?;
+
+        let jsAlbumArtists = ctx.empty_array();
 
         for (i, artist) in track.album_artists.into_iter().enumerate() {
-            let jsArtistString = JsString::new(scope, &artist).unwrap();
-            jsAlbumArtists.set(i as u32, jsArtistString)?;
+            let jsArtistString = ctx.string(&artist);
+            jsAlbumArtists.set(&mut ctx, i as u32, jsArtistString)?;
         }
-        jsTrack.set("albumArtists", jsAlbumArtists)?;
-        jsTrack.set("album", JsString::new(scope, &track.album).unwrap())?;
-        jsTrack.set("trackNumber", JsInteger::new(scope, track.track_number))?;
+
+        jsTrack.set(&mut ctx, "albumArtists", jsAlbumArtists)?;
+        let album = ctx.string(&track.album);
+        jsTrack.set(&mut ctx, "album", album)?;
+
+        let trackNumber = ctx.number(track.track_number);
+        jsTrack.set(&mut ctx, "trackNumber", trackNumber)?;
 
         match &track.musicbrainz_track_id {
-            Some(track_id) => jsTrack.set(
-                "musicbrainzTrackId",
-                JsString::new(scope, track_id).unwrap(),
-            ),
-            None => jsTrack.set("musicbrainzTrackId", JsNull::new()),
+            Some(track_id) => {
+                let trackId = ctx.string(track_id);
+                jsTrack.set(&mut ctx, "musicbrainzTrackId", trackId)
+            }
+            None => {
+                let null = ctx.null();
+                jsTrack.set(&mut ctx, "musicbrainzTrackId", null)
+            }
         }?;
 
-        jsTrack.set(
-            "hasFrontCover",
-            JsBoolean::new(scope, track.has_front_cover),
-        )?;
-        jsTrack.set(
-            "frontCoverHeight",
-            JsInteger::new(scope, track.front_cover_height),
-        )?;
-        jsTrack.set(
-            "frontCoverWidth",
-            JsInteger::new(scope, track.front_cover_width),
-        )?;
-        jsTrack.set("bitrate", JsInteger::new(scope, track.bitrate))?;
-        jsTrack.set("sampleRate", JsInteger::new(scope, track.sample_rate))?;
-        jsTrack.set("source", JsString::new(scope, &track.source).unwrap())?;
-        jsTrack.set("discNumber", JsInteger::new(scope, track.disc_number))?;
-        jsTrack.set("duration", JsInteger::new(scope, track.duration))?;
-        jsTrack.set("fileType", JsInteger::new(scope, track.file_type.to_i32().unwrap()))?;
-        jsTrack.set("updated", JsString::new(scope, &track.updated).unwrap())?;
-        jsTracks.set(i as u32, jsTrack)?;
+        let hasFrontCover = ctx.boolean(track.has_front_cover);
+        jsTrack.set(&mut ctx, "hasFrontCover", hasFrontCover)?;
+
+        let frontCoverHeight = ctx.number(track.front_cover_height);
+        jsTrack.set(&mut ctx, "frontCoverHeight", frontCoverHeight)?;
+
+        let frontCoverWidth = ctx.number(track.front_cover_width);
+        jsTrack.set(&mut ctx, "frontCoverWidth", frontCoverWidth)?;
+
+        let bitrate = ctx.number(track.bitrate);
+        jsTrack.set(&mut ctx, "bitrate", bitrate)?;
+
+        let sampleRate = ctx.number(track.sample_rate);
+        jsTrack.set(&mut ctx, "sampleRate", sampleRate)?;
+
+        let source = ctx.string(&track.source);
+        jsTrack.set(&mut ctx, "source", source)?;
+
+        let discNumber = ctx.number(track.disc_number);
+        jsTrack.set(&mut ctx, "discNumber", discNumber)?;
+
+        let duration = ctx.number(track.duration);
+        jsTrack.set(&mut ctx, "duration", duration)?;
+
+        let fileType = ctx.number(track.file_type.to_i32().unwrap());
+        jsTrack.set(&mut ctx, "fileType", fileType)?;
+
+        let updated = ctx.string(&track.updated);
+        jsTrack.set(&mut ctx, "updated", updated)?;
+
+        jsTracks.set(&mut ctx, i as u32, jsTrack)?;
     }
-    ret.set("tracks", jsTracks)?;
+    ret.set(&mut ctx, "tracks", jsTracks)?;
     Ok(ret)
 }
 
-register_module!(m, {
-    m.export("queryTracks", query_tracks);
-    m.export("refreshTracks", refresh_tracks)
+register_module!(mut m, {
+    m.export_function("queryTracks", query_tracks);
+    m.export_function("refreshTracks", refresh_tracks)
 });
