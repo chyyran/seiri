1b6ef9819827c76dd79aa9986c18e857f8d91a81
Ronny Chan <ronny6993@gmail.com>
2018-12-13 16:54:18 -0500

    Properly join the watcher thread when qutiting.

--- seiri-watcher/src/main.rs
+++ seiri-watcher/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(toowned_clone_into)]
 
-use crossbeam::channel::{unbounded, Receiver};
+use crossbeam::channel::{select, unbounded, Receiver, Sender};
 use leak::Leak;
 
 use std::borrow::Cow;
@@ -8,9 +8,9 @@ use std::ffi::OsStr;
 use std::io;
 use std::net::TcpListener;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-use std::sync::Arc;
 
 mod utils;
 mod watcher;
@@ -38,7 +38,11 @@ fn process(path: &Path, config: &Config, conn: &Connection, retry: bool) {
             Ok(track) => match paths::move_new_track(&track, &library_path.0, &library_path.1) {
                 Ok(track) => {
                     database::add_track(&track, conn);
-                    eprintln!("TRACKADDED::{}||{}", track.artist.trim(), track.title.trim());
+                    eprintln!(
+                        "TRACKADDED::{}||{}",
+                        track.artist.trim(),
+                        track.title.trim()
+                    );
                 }
                 Err(_) if retry => process(path, config, conn, false),
                 Err(Error::UnableToMove(_)) => {
@@ -97,15 +101,19 @@ fn begin_watch(config: &'static Config, pool: Arc<ConnectionPool>, rx: &Receiver
     }
 }
 
-fn get_watcher_thread(rx: Receiver<WatchStatus>, config: &'static Config, pool: Arc<ConnectionPool>) -> io::Result<thread::JoinHandle<()>> {
+fn get_watcher_thread(
+    rx: Receiver<WatchStatus>,
+    config: &'static Config,
+    pool: Arc<ConnectionPool>,
+) -> io::Result<thread::JoinHandle<()>> {
     thread::Builder::new()
         .name("WatchThread".to_string())
-        .spawn(move || {
-            begin_watch(config, pool, &rx)
-        })
+        .spawn(move || begin_watch(config, pool, &rx))
 }
 
-fn start_watcher_watchdog(wait_time: Duration, config: &'static Config, pool: Arc<ConnectionPool>) {
+fn start_watcher_watchdog(wait_time: Duration, config: &'static Config, pool: Arc<ConnectionPool>) -> Sender<()> {
+    let (qtx, qrx) = unbounded::<()>();
+
     thread::spawn(move || {
         let (tx, rx) = unbounded();
         let mut tx = tx;
@@ -113,28 +121,43 @@ fn start_watcher_watchdog(wait_time: Duration, config: &'static Config, pool: Ar
         wait_for_watch_root_available(&config.music_folder);
         let mut _watch_thread = get_watcher_thread(rx, config, Arc::clone(&pool)).unwrap();
         loop {
-            thread::park_timeout(wait_time);
-            if tx.send(WatchStatus::KeepAlive).is_err() {
-                eprintln!("EWATCHERDIED::Keep-alive failed. Watcher thread probably panicked. Restarting Watcher Thread...");
-                let (new_tx, rx) = unbounded();
-                tx = new_tx.clone();
-                _watch_thread = get_watcher_thread(rx, config, Arc::clone(&pool)).unwrap();
-            }
+            select! {
+                recv(qrx) -> _ => {
+                    // do quit stuff
+                    if tx.send(WatchStatus::Exit).is_ok() {
+                        match _watch_thread.join() {
+                            _ => ()
+                        }
+                    }
+                    drop(pool);
+                    break;
+                },
+                default(wait_time) => {
+                    if tx.send(WatchStatus::KeepAlive).is_err() {
+                        eprintln!("EWATCHERDIED::Keep-alive failed. Watcher thread probably panicked. Restarting Watcher Thread...");
+                        let (new_tx, rx) = unbounded();
+                        tx = new_tx.clone();
+                        _watch_thread = get_watcher_thread(rx, config, Arc::clone(&pool)).unwrap();
+                    }
 
-            let music_folder = paths::ensure_music_folder(&config.music_folder);
-            if music_folder.is_err() {
-                eprintln!("EWATCHERNOACCESS::{}", &config.music_folder);
-                wait_for_watch_root_available(&config.music_folder);
-                let (new_tx, rx) = unbounded();
-                tx.send(WatchStatus::Exit).unwrap();
-                eprintln!(
-                    "EWATCHERRESTART::Requested watcher thread exit. Restarting Watcher Thread..."
-                );
-                tx = new_tx.clone();
-                _watch_thread = get_watcher_thread(rx, config, Arc::clone(&pool)).unwrap();
+                    let music_folder = paths::ensure_music_folder(&config.music_folder);
+                    if music_folder.is_err() {
+                        eprintln!("EWATCHERNOACCESS::{}", &config.music_folder);
+                        wait_for_watch_root_available(&config.music_folder);
+                        let (new_tx, rx) = unbounded();
+                        tx.send(WatchStatus::Exit).unwrap();
+                        eprintln!(
+                            "EWATCHERRESTART::Requested watcher thread exit. Restarting Watcher Thread..."
+                        );
+                        tx = new_tx.clone();
+                        _watch_thread = get_watcher_thread(rx, config, Arc::clone(&pool)).unwrap();
+                    }
+                }
             }
         }
     });
+
+    qtx
 }
 
 fn ensure_port(port: u16) -> Result<TcpListener, io::Error> {
@@ -145,7 +168,7 @@ fn ensure_port(port: u16) -> Result<TcpListener, io::Error> {
 }
 
 fn main() {
-    let _lock = ensure_port(9235).expect("ENOLOCK::Unable to acquire lock");
+    let _lock = ensure_port(9235).expect("ENOLOCK::Unable to acquire lock. Only have one instance of seiri running.");
 
     let wait_time = Duration::from_secs(5);
     match config::get_config() {
@@ -156,9 +179,10 @@ fn main() {
             let pool = database::get_connection_pool();
             let db_pool = Arc::new(pool);
             //let config = Arc::new(config);
-            start_watcher_watchdog(wait_time, config, Arc::clone(&db_pool));
+            let quit_handle = start_watcher_watchdog(wait_time, config, Arc::clone(&db_pool));
             let conn = database::get_database_connection();
             utils::wait_for_exit(&conn, config);
+            quit_handle.send(()).unwrap();
             drop(conn);
             drop(db_pool);
         }
@@ -175,5 +199,4 @@ fn main() {
             }
         }
     }
- 
 }
