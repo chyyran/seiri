c94e0babae9e6ac8cbf24bd3be1ea3ca73ee11b8
Ronny Chan <ronny6993@gmail.com>
2020-12-23 14:43:58 -0500

    seiri-neon: slightly better error handling

--- seiri-neon/native/src/lib.rs
+++ seiri-neon/native/src/lib.rs
@@ -60,79 +60,88 @@ fn query_tracks(mut ctx: FunctionContext) -> JsResult<JsObject> {
 
     let bang = Bang::new(&query).unwrap();
     let conn = database::get_database_connection();
-    let results: Vec<Track> = database::query_tracks(bang, &conn, None, None).unwrap();
-    let jsTracks = ctx.empty_array();
+    let results = database::query_tracks(bang, &conn, None, None);
 
-    for (i, track) in results.into_iter().enumerate() {
-        let jsTrack = ctx.empty_object();
-        let filePath = ctx.string(&track.file_path.into_os_string().into_string().unwrap());
-        jsTrack.set(&mut ctx, "filePath", filePath)?;
+    let result: JsResult<JsObject> = match results {
+        Ok(results) => {
+            let jsTracks = ctx.empty_array();
 
-        let title = ctx.string(&track.title);
-        jsTrack.set(&mut ctx, "title", title)?;
-
-        let artist = ctx.string(&track.artist);
-        jsTrack.set(&mut ctx, "artist", artist)?;
-
-        let jsAlbumArtists = ctx.empty_array();
-
-        for (i, artist) in track.album_artists.into_iter().enumerate() {
-            let jsArtistString = ctx.string(&artist);
-            jsAlbumArtists.set(&mut ctx, i as u32, jsArtistString)?;
+            for (i, track) in results.into_iter().enumerate() {
+                let jsTrack = ctx.empty_object();
+                let filePath = ctx.string(&track.file_path.into_os_string().into_string().unwrap());
+                jsTrack.set(&mut ctx, "filePath", filePath)?;
+        
+                let title = ctx.string(&track.title);
+                jsTrack.set(&mut ctx, "title", title)?;
+        
+                let artist = ctx.string(&track.artist);
+                jsTrack.set(&mut ctx, "artist", artist)?;
+        
+                let jsAlbumArtists = ctx.empty_array();
+        
+                for (i, artist) in track.album_artists.into_iter().enumerate() {
+                    let jsArtistString = ctx.string(&artist);
+                    jsAlbumArtists.set(&mut ctx, i as u32, jsArtistString)?;
+                }
+        
+                jsTrack.set(&mut ctx, "albumArtists", jsAlbumArtists)?;
+                let album = ctx.string(&track.album);
+                jsTrack.set(&mut ctx, "album", album)?;
+        
+                let trackNumber = ctx.number(track.track_number);
+                jsTrack.set(&mut ctx, "trackNumber", trackNumber)?;
+        
+                match &track.musicbrainz_track_id {
+                    Some(track_id) => {
+                        let trackId = ctx.string(track_id);
+                        jsTrack.set(&mut ctx, "musicbrainzTrackId", trackId)
+                    }
+                    None => {
+                        let null = ctx.null();
+                        jsTrack.set(&mut ctx, "musicbrainzTrackId", null)
+                    }
+                }?;
+        
+                let hasFrontCover = ctx.boolean(track.has_front_cover);
+                jsTrack.set(&mut ctx, "hasFrontCover", hasFrontCover)?;
+        
+                let frontCoverHeight = ctx.number(track.front_cover_height);
+                jsTrack.set(&mut ctx, "frontCoverHeight", frontCoverHeight)?;
+        
+                let frontCoverWidth = ctx.number(track.front_cover_width);
+                jsTrack.set(&mut ctx, "frontCoverWidth", frontCoverWidth)?;
+        
+                let bitrate = ctx.number(track.bitrate);
+                jsTrack.set(&mut ctx, "bitrate", bitrate)?;
+        
+                let sampleRate = ctx.number(track.sample_rate);
+                jsTrack.set(&mut ctx, "sampleRate", sampleRate)?;
+        
+                let source = ctx.string(&track.source);
+                jsTrack.set(&mut ctx, "source", source)?;
+        
+                let discNumber = ctx.number(track.disc_number);
+                jsTrack.set(&mut ctx, "discNumber", discNumber)?;
+        
+                let duration = ctx.number(track.duration);
+                jsTrack.set(&mut ctx, "duration", duration)?;
+        
+                let fileType = ctx.number(track.file_type.to_i32().unwrap());
+                jsTrack.set(&mut ctx, "fileType", fileType)?;
+        
+                let updated = ctx.string(&track.updated);
+                jsTrack.set(&mut ctx, "updated", updated)?;
+        
+                jsTracks.set(&mut ctx, i as u32, jsTrack)?;
+            }
+            ret.set(&mut ctx, "tracks", jsTracks)?;
+            Ok(ret)
         }
-
-        jsTrack.set(&mut ctx, "albumArtists", jsAlbumArtists)?;
-        let album = ctx.string(&track.album);
-        jsTrack.set(&mut ctx, "album", album)?;
-
-        let trackNumber = ctx.number(track.track_number);
-        jsTrack.set(&mut ctx, "trackNumber", trackNumber)?;
-
-        match &track.musicbrainz_track_id {
-            Some(track_id) => {
-                let trackId = ctx.string(track_id);
-                jsTrack.set(&mut ctx, "musicbrainzTrackId", trackId)
-            }
-            None => {
-                let null = ctx.null();
-                jsTrack.set(&mut ctx, "musicbrainzTrackId", null)
-            }
-        }?;
-
-        let hasFrontCover = ctx.boolean(track.has_front_cover);
-        jsTrack.set(&mut ctx, "hasFrontCover", hasFrontCover)?;
-
-        let frontCoverHeight = ctx.number(track.front_cover_height);
-        jsTrack.set(&mut ctx, "frontCoverHeight", frontCoverHeight)?;
-
-        let frontCoverWidth = ctx.number(track.front_cover_width);
-        jsTrack.set(&mut ctx, "frontCoverWidth", frontCoverWidth)?;
-
-        let bitrate = ctx.number(track.bitrate);
-        jsTrack.set(&mut ctx, "bitrate", bitrate)?;
-
-        let sampleRate = ctx.number(track.sample_rate);
-        jsTrack.set(&mut ctx, "sampleRate", sampleRate)?;
-
-        let source = ctx.string(&track.source);
-        jsTrack.set(&mut ctx, "source", source)?;
-
-        let discNumber = ctx.number(track.disc_number);
-        jsTrack.set(&mut ctx, "discNumber", discNumber)?;
-
-        let duration = ctx.number(track.duration);
-        jsTrack.set(&mut ctx, "duration", duration)?;
-
-        let fileType = ctx.number(track.file_type.to_i32().unwrap());
-        jsTrack.set(&mut ctx, "fileType", fileType)?;
-
-        let updated = ctx.string(&track.updated);
-        jsTrack.set(&mut ctx, "updated", updated)?;
-
-        jsTracks.set(&mut ctx, i as u32, jsTrack)?;
-    }
-    ret.set(&mut ctx, "tracks", jsTracks)?;
-    Ok(ret)
+        Err(e) => {
+            ctx.throw_error(e.to_string())
+        }
+    };
+    result
 }
 
 register_module!(mut m, {
